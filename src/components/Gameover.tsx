05e8873793a376bf451c5ea07c2a4d57cd9f8090
lotemelazar <elazarlotem1808@gmail.com>
2023-03-05 18:52:10 +0200

    update

--- src/components/Gameover.tsx
+++ src/components/Gameover.tsx
@@ -5,7 +5,7 @@ function GameOver(): JSX.Element {
   const { gameOver, currAttempt, correctWord } = useContext(AppContext);
   return (
     <div className="gameOver">
-      <h3>{gameOver.guessed ? 'You Correctly Guessed The Wordle' : 'You Failed To Guess The Word'}</h3>
+      <h3>{gameOver.guessed ? 'You Correctly Guessed The Word' : 'You Failed To Guess The Word'}</h3>
       <h1>Correct Word: {correctWord.toUpperCase()}</h1>
       {gameOver.guessed && <h3>You guessed in {currAttempt.rowNum} attempts</h3>}
     </div>
